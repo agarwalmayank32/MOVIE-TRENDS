2ef67e3c448b72bcbd89056882302a2bb344caa6
mayank <agarwal.mayank32@gmail.com>
2016-05-29 22:50:48 +0530

    Almost Done

---
+++ app/src/main/java/com/udacity/popularmovies/Utils/MovieUtils.java
@@ -0,0 +1,14 @@
+package com.udacity.popularmovies.Utils;
+
+public class MovieUtils {
+
+    public static String BASE_MOVIE_URL="http://api.themoviedb.org/3/movie/";
+    public static String API_KEY="?api_key=40986cc3f374e9d7588765a24ee28d52";
+    public static String BASE_PICTURE_URL="http://image.tmdb.org/t/p/";
+    public static String PICTURE_SIZE1="w185/";
+    public static String PICTURE_SIZE2="w780/";
+    public static String BASE_TRAILER_URL="https://www.youtube.com/watch?v=";
+    public static String Videos = "/videos";
+    public static String Reviews = "/reviews";
+
+}
